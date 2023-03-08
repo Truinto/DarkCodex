f1d245578100de88a947b0712fda46b85c8dff4d
Teraunce <teraunce@gmail.com>
2023-03-07 20:25:55 -0500

    Add files via upload

--- DarkCodex/Patches/Patch_ProdigiousTWF.cs
+++ DarkCodex/Patches/Patch_ProdigiousTWF.cs
@@ -20,12 +20,13 @@ namespace DarkCodex
             data.Last().Rewind(f => f.IsStloc(7));
 
             // set variable to true, if weapon should be considered light
-            data.InsertAfter(Patch);
+            //data.InsertAfter(PatchTWF);
+			//The above throws an OpCode conversion error. It's ambiguous which "InsertAfter" it is calling so it goes for the first one and not the Delegate one. --Teraunce
 
             return data.Code;
         }
 
-        public static void Patch(TwoWeaponFightingAttackPenalty __instance, [LocalParameter(type: typeof(bool), index: 7)] ref bool flag)
+        public static void PatchTWF(TwoWeaponFightingAttackPenalty __instance, [LocalParameter(type: typeof(bool), index: 7)] ref bool flag)
         {
             flag = flag || __instance.Owner.HasFlag(MechanicFeature.ProdigiousTWF);
         }
